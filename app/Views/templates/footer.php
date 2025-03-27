4832fdf758b301eaac640786ad6207616b37a5b7
Rafael Gonçalves <jrafaelg@live.com>
2025-03-27 18:19:14 -0300

    correções do alert

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,22 +1,12 @@
-
-
-
-
-
-
-
-
-
-
-
-    <div class="row">
-        <div class="col-md-12 text-center">
-            <em class="text-center">&copy; 2022</em>
-        </div>
+<div class="row">
+    <div class="col-md-12 text-center">
+        <em class="text-center">&copy; 2022</em>
     </div>
+</div>
 
-    <script src="/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/common.js"></script>
 </body>
+
 </html>
-
-
