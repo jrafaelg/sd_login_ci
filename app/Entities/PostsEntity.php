261d8c99da6dac81b700bd272ac276058337c8cf
Rafael Gonçalves <jrafaelg@live.com>
2025-05-14 18:14:51 -0300

    .

--- app/Entities/PostsEntity.php
+++ app/Entities/PostsEntity.php
@@ -10,12 +10,29 @@ class PostsEntity extends Entity
     protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
     protected $casts   = [];
 
+
+    /**
+     * --------------------------------------------------------------------------
+     * FUNÇÃO PARA FORMATAÇÃO DO CPF EM PYTHON
+     * --------------------------------------------------------------------------
+     * def __str__(self) -> str:
+     *      verificador = self._cpf%100
+     *      atual = self._cpf//100
+     *      terc = atual%1000
+     *      atual = atual//1000
+     *      seg = atual%1000
+     *      prim = atual//1000
+     *      return f"{self.nome} {prim:03d}.{seg:03d}.{terc:03d}-{verificador:02d}"
+     * 
+     * PHP: intdiv(int $num1, int $num2): int
+     * 
+     */
+
+
+
+
     public function getTitle()
     {
-        //$title = $this->attributes['title'];
-
-        //dd(strtoupper($this->attributes['title']));
-        // $this->attributes['created_at'] = $this->mutateDate($this->attributes['created_at']);
         $this->attributes['title'] = ucwords($this->attributes['title']);
 
         return $this->attributes['title'];
